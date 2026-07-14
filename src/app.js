a758565c8cf9ff81bd08b6f7f99c1f499563bec8
muzamilCodes <warmuzamil68@gmail.com>
2026-07-14 19:04:32 +0530

    app

--- src/app.js
+++ src/app.js
@@ -91,9 +91,14 @@ export function createApp() {
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
 
+  app.get('/', (_, res) => {
+    res.json({ ok: true, service: 'zaika-api' });
+  });
+
   app.get('/api/health', (_, res) => {
     res.json({ ok: true, service: 'zaika-api' });
   });
+
 
   app.use('/api/auth', authRoutes);
   app.use('/api/users', userRoutes);
